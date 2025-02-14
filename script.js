5de526f1bcf852a9feb3525f92f44d7036821c91
Shubham Pandit <Shubhampandit75572@gmail.com>
2025-02-14 13:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
+function getFormvalue() {https://www.svgrepo.com/show/345221/three-dots.svg
     //Write your code here
+	let fname = document.forms["form1"]["fname"].value.trim()
+	let lname = document.forms["form1"]['lname'].value.trim()
 
+	if(fname === '' || lname === ''){
+		alert("Both First Name and Last Name must be filled out!")
+		return false
+	}
+
+	alert(fname + " " + lname)
+	return false
 }
